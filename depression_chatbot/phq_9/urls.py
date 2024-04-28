2fcc9e2a7118bd110b22361bda19718aa190a132
AmannSingh02 <amansingh941941@gmail.com>
2024-04-29 01:33:44 +0530

    score fetch api added

--- depression_chatbot/phq_9/urls.py
+++ depression_chatbot/phq_9/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import PHQ9QuestionList, PHQResponseCreate
+from .views import PHQ9QuestionList, PHQResponseCreate, PHQScore
 
 urlpatterns = [
     path('api/questions/', PHQ9QuestionList.as_view(), name='question-list'),
     path('api/responses/', PHQResponseCreate.as_view(), name='response-create'),
+    path('api/score/<int:user_id>/', PHQScore.as_view(), name='score'),
 ]
